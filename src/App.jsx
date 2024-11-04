8f7b647ea5995c190aca1085b59c0f360378f6bc
o-Meli-o <m.hejranfar@gmail.com>
2024-11-04 23:34:25 +0330

    Lesson1
    a simple header footer testing.

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Header from './Header.jsx';
+import Footer from './Footer.jsx';
+
+function App() {
+  return(
+    <>
+    <Header></Header>
+    <Footer></Footer>
+    </>
+    
+  );
+}
+
+export default App
